b4d52a83f68c963f9b6cbb731aa695da4b5d9f6f
Jiadeng Ruan <jiadeng@16financial.com>
2018-10-19 21:06:55 +0800

    add Formatter

--- objectify_json.py
+++ objectify_json.py
@@ -52,3 +52,51 @@ class ObjectifyJSON:
 def get_data_by_path(data, path):
     _o = ObjectifyJSON(data)
     return eval("_o{}".format(path))._data
+
+
+class Formatter:
+    context = ObjectifyJSON(None)
+
+    def format_value(self, value: str):
+        return value.format(self=self.context)
+
+    def parse_dict(self, data: dict):
+        for k, v in data.copy().items():
+            if isinstance(v, dict):
+                v = self.parse_dict(v)
+            elif isinstance(v, list):
+                v = self.parse_list(v)
+            elif isinstance(v, (int, float, bool)):
+                v = v
+            elif isinstance(v, str):
+                v = self.format_value(v)
+
+            data[self.format_value(k)] = v
+        return data
+
+    def parse_list(self, data: list):
+        for i, v in enumerate(data.copy()):
+            if isinstance(v, dict):
+                v = self.parse_dict(v)
+            elif isinstance(v, list):
+                v = self.parse_list(v)
+            elif isinstance(v, (int, float, bool)):
+                v = v
+            elif isinstance(v, str):
+                v = self.format_value(v)
+
+            data[i] = v
+        return data
+
+    def parse_cofig(self, data):
+        # get the real data
+        if isinstance(data, ObjectifyJSON):
+            data = data._data
+
+        if isinstance(data, (list, tuple)):
+            return self.parse_list(data)
+        if isinstance(data, dict):
+            return self.parse_dict(data)
+        if isinstance(data, str):
+            return self.format_value(data)
+        raise NotImplementedError(str(type(data)))
